e8b8e3a8ce4fb5be24e82384db561ae5f21bfd93
Kevin Montoya <kmontocam@outlook.com>
2024-12-10 09:27:34 -0600

    chore: four space indent groovy script

--- cicd/jobs/main.groovy
+++ cicd/jobs/main.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 folder('kmontocam.com') {
-  displayName('kmontocam.com')
-  description('Folder to store kmontocam.com specific items')
+    displayName('kmontocam.com')
+    description('Folder to store kmontocam.com specific items')
 }
